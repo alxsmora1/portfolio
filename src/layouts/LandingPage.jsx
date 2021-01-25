a4f423dc7d32b72422a367fbd6a24f0b9c99faca
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2021-01-24 18:58:05 -0600

    Add a LandingPage Component Basic React Class Component

---
+++ src/layouts/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
