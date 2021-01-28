b3c08376560bd93cd3f98203c1b0935cffb8184a
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2021-01-27 22:58:56 -0600

    Add portfolio component and AOS

--- src/layouts/LandingPage.jsx
+++ src/layouts/LandingPage.jsx
@@ -3,14 +3,21 @@ import React, { Component } from 'react';
 import Navbarx from '../components/Navbarx';
 import Masthead from '../components/Masthead';
 import AboutMe from '../components/AboutMe';
+import Portfolio from '../components/Portfolio';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 class LandingPage extends Component {
+
     render() {
         return (
             <>
                 <Navbarx />
                 <Masthead />
                 <AboutMe />
+                <Portfolio />
             </>
         );
     }
