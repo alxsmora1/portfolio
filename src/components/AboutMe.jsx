2c6122d8c3eaafe76aaa17ffe7fbbd7e1fd3b688
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2021-08-01 02:25:19 -0500

    Add effects and reorganize the stack card
    Add icons for every languague and framework of my stack.
    Add some AOS effects on the about me section cards.
    Add the cristal effect to the cards in the section about me.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 
-import { Card, CardText, CardBody, CardTitle, List } from "reactstrap";
+import { Row, Col, Card, CardText, CardBody, CardTitle, List } from "reactstrap";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCode,
   faCube,
-  faCubes,
   faStar,
+  faDatabase,
+  faTerminal,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   faLinux,
@@ -16,6 +17,12 @@ import {
   faPython,
   faCss3,
   faHtml5,
+  faReact,
+  faAngular,
+  faVuejs,
+  faSass,
+  faBootstrap,
+  faSymfony,
 } from "@fortawesome/free-brands-svg-icons";
 
 class AboutMe extends Component {
@@ -34,8 +41,8 @@ class AboutMe extends Component {
             <div class="divider-custom-line"></div>
           </div>
           <div class="row justify-content-center">
-            <div class="col-lg-4">
-              <Card className="shadow mb-3 bottom-color">
+            <div class="col-md-4" data-aos="zoom-in">
+              <Card className="shadow mb-3  crystal-card">
                 <CardBody>
                   <CardTitle tag="h5" className="text-center">
                     ME
@@ -65,13 +72,12 @@ class AboutMe extends Component {
                 </CardBody>
               </Card>
             </div>
-            <div class="col-lg-4">
-              <Card className="shadow mb-3 bottom-color">
+            <div class="col-md-3" data-aos="zoom-in">
+              <Card className="shadow mb-3  crystal-card">
                 <CardBody>
                   <CardTitle tag="h5" className="text-center">
                     SKILLS
                   </CardTitle>
-                  <CardText>
                     <List type="unstyled">
                       <li>
                         <FontAwesomeIcon icon={faCube} /> Adaptability
@@ -100,43 +106,82 @@ class AboutMe extends Component {
                         of Bash/Shell
                       </li>
                     </List>
-                  </CardText>
                 </CardBody>
               </Card>
             </div>
-            <div class="col-lg-4">
-              <Card className="shadow mb-3 bottom-color">
+            <div class="col-md-5" data-aos="zoom-in">
+              <Card className="shadow mb-3 px-2 crystal-card">
                 <CardBody>
                   <CardTitle tag="h5" className="text-center">
                     STACK
                   </CardTitle>
-                  <CardText>
+                  <Row>
+                    <Col md="4" className="text-center">
                     <List type="unstyled">
                       <li>
-                        <FontAwesomeIcon icon={faPhp} /> PHP (Symfony)
+                        <FontAwesomeIcon icon={faJsSquare} size="2x" /> <br /> Javascript
                       </li>
+                      <br />
                       <li>
-                        <FontAwesomeIcon icon={faPython} /> Python (Django)
+                        <FontAwesomeIcon icon={faPhp} size="2x" /> <br /> PHP 
                       </li>
+                      <br />
                       <li>
-                        <FontAwesomeIcon icon={faJsSquare} /> JS (ReactJS)
+                        <FontAwesomeIcon icon={faPython} size="2x" /> <br /> Python
                       </li>
+                      <br />
                       <li>
-                        <FontAwesomeIcon icon={faCubes} /> SQL
-                        (Mariadb/PostgreSQL)
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faCss3} /> CSS 3 (Bootstrap)
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faHtml5} /> HTML 5
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faLinux} /> Linux (Debian,
-                        Ubuntu, Raspbian)
-                      </li>
+                        <FontAwesomeIcon icon={faDatabase} size="2x" /> <br /> SQL
+                      </li>                  
                     </List>
-                  </CardText>
+                    </Col>
+                    <Col md="4" className="text-center">
+                      <List type="unstyled">
+                      <li>
+                        <FontAwesomeIcon icon={faReact} size="2x" /> <br /> ReactJS
+                      </li>
+                      <br />
+                      <li>
+                        <FontAwesomeIcon icon={faAngular} size="2x" /> <br /> Angular
+                      </li>
+                      <br />
+                      <li>
+                        <FontAwesomeIcon icon={faVuejs} size="2x" /> <br /> Vue
+                      </li>
+                      <br />
+                      <li>
+                        <FontAwesomeIcon icon={faSymfony} size="2x" /> <br /> Symfony
+                      </li>
+                      <br />
+                      <li>
+                        <FontAwesomeIcon icon={faBootstrap} size="2x" /> <br /> Bootstrap
+                      </li>
+                      </List>
+                    </Col>
+                    <Col md="4" className="text-center">
+                      <List type="unstyled">
+                      <li>
+                        <FontAwesomeIcon icon={faHtml5} size="2x" /> <br /> HTML 5
+                      </li>
+                      <br />
+                      <li>
+                        <FontAwesomeIcon icon={faCss3} size="2x" /> <br /> CSS 3 
+                      </li>    
+                      <br />
+                      <li>
+                        <FontAwesomeIcon icon={faSass} size="2x" /> <br /> Sass
+                      </li>
+                      <br />
+                      <li>
+                        <FontAwesomeIcon icon={faLinux} size="2x" /> <br /> Linux
+                      </li>
+                      <br />
+                      <li>
+                        <FontAwesomeIcon icon={faTerminal} size="2x" /> <br /> Terminal
+                      </li>
+                      </List>
+                    </Col>
+                  </Row>
                 </CardBody>
               </Card>
             </div>
