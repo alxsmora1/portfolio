fb21d57ac9580815b1a8a8076438efa61f7c2022
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2021-08-01 22:18:05 -0500

    Structure change  and new props
    Change string contents to template literals.
    Add the prop for teh link of github code.
    Minor changes to styles.

--- src/components/IndividualProjects.jsx
+++ src/components/IndividualProjects.jsx
@@ -6,27 +6,29 @@ import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 
 class IndividualProjects extends Component {
-  showModal = (title, body, image, skills, link) => {
+  showModal = (title, body, image, skills, link, linkCode) => {
     const listItems = skills.map((skill) => " " + skill);
 
     const MySwal = withReactContent(Swal);
 
     MySwal.fire({
-      title: '<h1 class="text-center">' + title + "</h1>",
-      html:
-        '<img class="img-fluid text-center shadow rounded" src="' +
-        image +
-        '" alt="" />' +
-        '<p class="lead text-center"><strong>Description</strong></p>' +
-        '<p class="text-center lead">' +
-        body +
-        "</p>" +
-        '<p class="lead text-center"><strong>Build with</strong></P>' +
-        listItems +
-        "<br><br>" +
-        '<a href="' +
-        link +
-        '" target="_BLANK">Go to project</a>',
+      title: `<h1 class="text-center">${title}</h1>`,
+      html:`
+        <img class="img-fluid text-center shadow rounded" src="${image}" alt="" />
+        <p class="lead text-center"><strong>Description</strong></p>
+        <p class="text-center lead"> ${body} </p>
+        <p class="lead text-center"><strong>Build with: </strong></P>
+        ${listItems}
+        <br><br>
+        <div class="row">
+          <div class="col-md-6">
+            <a class="btn btn-primary form-control" href="${link}" target="_BLANK"> <i class="fas fa-globe"></i> Live Project</a>
+          </div>
+          <div class="col-md-6">
+            <a class="btn btn-primary form-control" href="${linkCode}" target="_BLANK"> <i class="fab fa-github"></i> Github Code</a>
+          </div>
+        </div>
+        `,
       showCloseButton: true,
       showConfirmButton: false,
     });
@@ -35,7 +37,7 @@ class IndividualProjects extends Component {
   render() {
     return (
       <div class="col-md-6 col-lg-4 mb-5">
-        <div class="portfolio-item mx-auto shadow h-100" data-aos="fade-up">
+        <div class="portfolio-item mx-auto shadow h-100" data-aos="flip-left">
           <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
             <div class="portfolio-item-caption-content text-center text-white">
               <p>{this.props.title}</p>
@@ -47,7 +49,8 @@ class IndividualProjects extends Component {
                     this.props.body,
                     this.props.image,
                     this.props.skills,
-                    this.props.link
+                    this.props.link,
+                    this.props.linkCode
                   )
                 }
               >
